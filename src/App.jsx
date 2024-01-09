e3fbeaf094f10d69a5fb06b9dfbf1f02b3fef113
pkstoy13 <pkstoyanov1@gmail.com>
2024-01-09 15:35:04 -0500

    installed tailwind onto project

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import cubeLogo from "./assets/blue 3d rectangle.svg";
 import triLogo from "./assets/triangle logo.svg";
 import plogo_1 from "./assets/pain-tings small stroke circle.svg";
 //import plogo_2 from "./assets/pain-tings logo.svg";
-import "./App.css";
 
-function App() {
+export function App() {
   //const [count, setCount] = useState(0);
 
   return (
@@ -21,29 +20,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>*/}
-      <h1>The Collection</h1>
-      {/*<div className="card">
+      <div className="h-screen bg-slate-600">
+        <h1>The Collection</h1>
+        {/*<div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        count is {count}
         </button>
-    </div>*/}
-      <p className="read-the-docs">
-        This is a collection of things I create in Affinity Design <br></br>{" "}
-        This is my first project using Vite instead of Create-React-App
-      </p>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <img src={cubeLogo} alt="Cube Logo" />
-        <img src={triLogo} alt="Triangle Logo" width="275px" />
-        <img src={plogo_1} alt="pain-tings Logo 1" />
+      </div>*/}
+        <p>
+          This is a collection of things I create in Affinity Design <br></br>{" "}
+          This is my first project using Vite instead of Create-React-App
+        </p>
+        <div>
+          <img src={cubeLogo} alt="Cube Logo" />
+          <img src={triLogo} alt="Triangle Logo" width="275px" />
+          <img src={plogo_1} alt="pain-tings Logo 1" />
+        </div>
       </div>
     </>
   );
 }
-
-export default App;
